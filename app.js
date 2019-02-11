72da9dbd01d16b6c105abb12e01bff07c5422d97
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2019-02-11 13:26:10 -0800

    users can sign up

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
+const dotenv = require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const expressValidator = require('express-validator');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/redditClone');
 const exphbs = require("express-handlebars");
 const app = express();
 
 
-const posts = require('./controllers/posts')
-const comments = require('./controllers/comments')
+
+
 // require('./data/reddit-db');
 
 //middleware
 app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'main'}));
 app.set('view engine', '.hbs');
 app.use(methodOverride("_method"));
+app.use(cookieParser())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,12 +28,17 @@ app.use(expressValidator());
 
 
 //routes
+const posts = require('./controllers/posts')
+const comments = require('./controllers/comments')
+const auth = require('./controllers/auth')
+
 app.get('/', (req, res) => {
     res.redirect('/posts')
 })
 
 app.use('/', posts)
 app.use('/', comments)
+app.use('/', auth)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
---
+++ controllers/auth.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
+router.get('/sign-up', (req, res) => {
+    res.render('sign-up')
+})
+
+router.post('/sign-up', (req, res) => {
+    const user = new User(req.body)
+    user.save()
+    .then(user => {
+        console.log(user._id)
+        let token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+        res.cookie("nToken", token, {maxAge: 900000, httpOnly: true})
+        return res.redirect('/')
+        
+    }).catch(err => {
+        console.log(err)
+        return res.status(400).send({ err : err.message })
+    })
+})
+
+
+module.exports = router
